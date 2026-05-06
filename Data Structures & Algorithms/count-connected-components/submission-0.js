32b6de6aced0c89c20a5b83a4d8835c930e2deb6
sushantDev <sushantsapkota83@gmail.com>
2026-05-06 13:40:32 +0545

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        const adj = Array.from({length: n}, () => []);
+        const visit = Array(n).fill(false);
+
+        for (const [u, v] of edges) {
+            adj[u].push(v);
+            adj[v].push(u);
+        }
+
+        const dfs = (node) => {
+            for (const nei of adj[node]) {
+                if (!visit[nei]) {
+                    visit[nei] = true;
+                    dfs(nei);
+                }
+            }
+        };
+
+        let res = 0;
+        for (let node = 0; node < n; node++) {
+            if (!visit[node]) {
+                visit[node] = true;
+                dfs(node);
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
