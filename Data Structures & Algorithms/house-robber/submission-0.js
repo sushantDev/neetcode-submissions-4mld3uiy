77d2b1b6473743702d2b96c10ebedfa918fef74f
sushantDev <sushantsapkota83@gmail.com>
2026-05-18 09:53:15 +0545

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        let rob1 = 0;
+        let rob2 = 0;
+
+        for (const num of nums) {
+            const temp = Math.max(num + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        
+        return rob2;
+    }
+}
