bb9d66d915bc1f87bfd4547a997564adb8b67046
sushantDev <sushantsapkota83@gmail.com>
2026-05-30 20:42:43 +0545

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+        const dp = new Array(s.length + 1).fill(false);
+
+        dp[s.length] = true;
+
+        for (let i = s.length - 1; i >= 0; i--) {
+            for (let word of wordDict) {
+                if (
+                    i + word.length <= s.length && 
+                    s.slice(i, i + word.length) === word
+                ) {
+                    dp[i] = dp[i + word.length];
+                }
+
+                if (dp[i]) break;
+            }
+        }
+
+        return dp[0];
+    }
+}
